ea3df04659e89e8ae00c4d0c7e1afbe81a92b42b
Lila Kurse <lilakurse@gmail.com>
2015-04-21 14:21:40 +0300

    Task_1: work with types

--- model/user/user.go
+++ model/user/user.go
@@ -1,15 +1,41 @@
 package user
-import "errors"
+import (
+	"errors"
+	"time"
+)
 //test user type
+
+type ContactType int
+const(
+ContactTypeIms ContactType = iota
+ContactTypePhone
+ContactTypeEmail
+ContactTypeAddress
+)
+
+type Contact struct{
+	Value string
+	Type ContactType
+}
+
 type User struct {
 	Name string `json:"username"`
+	Registration *time.Time
+	Age int
+	Birthday *time.Time
+	About string
+	Contacts *[]*Contact
 }
 
 const ErrorBadName  = "invalid name"
+const ErrorAge= "invalid value"
 
 func (u *User) Validate () error {
 	if len(u.Name) <= 2 {
 		return errors.New(ErrorBadName)
+	}
+	if u.Age <18  {
+		return errors.New(ErrorAge)
 	}
 	return nil
 }
