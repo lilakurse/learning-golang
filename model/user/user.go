3cce8f67621e376add43ddd86b2d8692ce57f3ae
Lila Kurse <lilakurse@gmail.com>
2015-04-21 15:22:56 +0300

    Task_1: work with types

--- model/user/user.go
+++ model/user/user.go
@@ -1,48 +1,50 @@
 package user
+
 import (
 	"errors"
 	"time"
 )
-//test user type
 
+//test user type
 type ContactType int
-const(
-ContactTypeIms ContactType = iota
-ContactTypePhone
-ContactTypeEmail
-ContactTypeAddress
+
+const (
+	ContactTypeIms ContactType = iota
+	ContactTypePhone
+	ContactTypeEmail
+	ContactTypeAddress
 )
 
-type Contact struct{
+type Contact struct {
 	Value string
-	Type ContactType
+	Type  ContactType
 }
 
 type User struct {
-	Name string `json:"username"`
+	Name         string `json:"username"`
 	Registration *time.Time
-	Age int
-	Birthday *time.Time
-	About string
-	Contacts *[]*Contact
+	Age          int
+	Birthday     *time.Time
+	About        string
+	Contacts     *[]*Contact
 }
 
-const ErrorBadName  = "invalid name"
-const ErrorAge= "invalid value"
+const ErrorBadName = "invalid name"
+const ErrorAge = "invalid value"
 
-func (u *User) Validate () error {
+func (u *User) Validate() error {
 	if len(u.Name) <= 2 {
 		return errors.New(ErrorBadName)
 	}
-	if u.Age <18  {
+	if u.Age < 18 {
 		return errors.New(ErrorAge)
 	}
 	return nil
 }
 
-func New () *User {
+func New() *User {
 	u := new(User)
-	u.Name = "Lola"
+	u.Name = "Lila"
+	u.Age = 20
 	return u
 }
-
--- model/user/user_test.go
+++ model/user/user_test.go
@@ -1,20 +1,22 @@
 package user
+
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestUser(t *testing.T) {
- 	u := New()
-	fmt.Print("User Name: ",u.Name,"\n")
-	if u.Name != "1ila" {
+	u := New()
+	fmt.Print("User Name: ", u.Name, "\n")
+	if u.Name != "Lila" {
 		t.Error("bad user name")
 	}
 }
 
 func TestUseTwo(t *testing.T) {
 	u := New()
-	fmt.Print("User Name: ",u.Name,"\n")
+	fmt.Print("User Name: ", u.Name, "\n")
+	fmt.Print("User Age: ", u.Age, "\n")
 	err := u.Validate()
 	if err != nil {
 		t.Error(err)
