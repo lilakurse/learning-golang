030c3638cdf43fe63e30c4949e5d304a7131c9d0
Lila Kurse <lilakurse@gmail.com>
2015-05-22 02:15:21 +0300

    add work with mgo

--- model/examples/registration.go
+++ model/examples/registration.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-//"fmt"
+	//"fmt"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"html/template"
 	"log"
 	"net/http"
-	"fmt"
+	"time"
 )
 
-func registration(w http.ResponseWriter, r *http.Request){
+func registration(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("C:/Users/Samsung/dev/go/src/github.com/lilakurse/learning-golang/model/examples/tmpl/registration"+ ".html")
+		t, _ := template.ParseFiles("C:/Users/Samsung/dev/go/src/github.com/lilakurse/learning-golang/model/examples/tmpl/registration" + ".html")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
@@ -20,10 +23,29 @@ func registration(w http.ResponseWriter, r *http.Request){
 		fmt.Println("email:", r.Form["email"])
 	}
 }
+
+type Person struct {
+	Name     string
+	Phone    string
+	email    string
+	birthday *time.Time
+}
+
 func main() {
 	http.HandleFunc("/registration", registration)
 	err := http.ListenAndServe(":8080", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	//work with mgo, open session
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("data").C("people")
+	err = c.Insert()
+
 }
